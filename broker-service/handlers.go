bdf4034ac1fccd579b2f4ab6528ca07d2101f30f
baniyapratik <baniyapratik@gmail.com>
2023-02-04 21:56:50 -0800

    implement json helpers and reformat handler

--- broker-service/handlers.go
+++ broker-service/handlers.go
@@ -1,24 +1,13 @@
 package main
 
 import (
-	"encoding/json"
 	"net/http"
 )
-
-type APIResponse struct {
-	Error   bool        `json:"error"`
-	Message string      `json:"message"`
-	Data    interface{} `json:"data,omitempty"`
-}
 
 func (app *Config) Broker(w http.ResponseWriter, r *http.Request) {
 	payload := APIResponse{
 		Error:   false,
 		Message: "Hi from broker",
 	}
-
-	out, _ := json.MarshalIndent(payload, "", "\t")
-	w.Header().Set("Content-type", "application/json")
-	w.WriteHeader(http.StatusAccepted)
-	w.Write(out)
+	app.writeJSON(w, http.StatusOK, payload)
 }
