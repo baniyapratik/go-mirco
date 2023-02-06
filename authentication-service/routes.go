aa3954b690904efbb44e0d294b9cdb3428acdda3
baniyapratik <baniyapratik@gmail.com>
2023-02-05 19:26:46 -0800

    adding route to auth-service

--- authentication-service/routes.go
+++ authentication-service/routes.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/rs/cors"
 	"net/http"
 )
@@ -10,5 +11,7 @@ func (app *Config) routes() http.Handler {
 	mux := chi.NewRouter()
 	mux.Use(cors.Default().Handler)
 
+	mux.Use(middleware.Heartbeat("/health"))
+	mux.Post("/authenticate", app.Authenticate)
 	return mux
 }
